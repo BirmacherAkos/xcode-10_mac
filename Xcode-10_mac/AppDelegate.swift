611d16c34baa981a627c4b83572fd4f7e075e574
Akos Birmacher <akos.birmacher@bitrise.io>
2019-04-30 13:37:35 +0200

    init

---
+++ Xcode-10_mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode-10_mac
+//
+//  Created by Akos Birmacher on 2019. 04. 30..
+//  Copyright © 2019. Bitrise. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode-10_mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode-10_mac
+//
+//  Created by Akos Birmacher on 2019. 04. 30..
+//  Copyright © 2019. Bitrise. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
